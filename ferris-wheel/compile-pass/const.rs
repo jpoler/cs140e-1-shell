2aa42bd4560ad470a604c170728ff4999002b7aa
Jon Poler <jonathan.poler@gmail.com>
2018-08-31 00:40:51 -0700

    Const fn

--- ferris-wheel/compile-pass/const.rs
+++ ferris-wheel/compile-pass/const.rs
@@ -1,8 +1,9 @@
+#![feature(const_fn)]
 // FIXME: Make me compile! Diff budget: 3 lines.
 
 const VAR: i32 = add(34, 10);
 
-fn add(a: i32, b: i32) -> i32 {
+const fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
