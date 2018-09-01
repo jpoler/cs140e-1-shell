43e744ccb51f743526e7360904a151f5ddb264de
Jon Poler <jonathan.poler@gmail.com>
2018-09-01 01:58:49 -0700

    trait-impl

--- ferris-wheel/run-pass/trait-impl.rs
+++ ferris-wheel/run-pass/trait-impl.rs
@@ -1,9 +1,28 @@
 // FIXME: Make me pass! Diff budget: 25 lines.
 
+#[derive(Debug)]
 enum Duration {
     MilliSeconds(u64),
     Seconds(u32),
     Minutes(u16)
+}
+
+use Duration::*;
+
+impl Duration {
+  fn to_millis(&self) -> u64 {
+    match self {
+      &MilliSeconds(millis) => millis,
+      &Seconds(secs) => secs as u64 *1000 as u64,
+      &Minutes(mins) => mins as u64*60*1000,
+    }
+  }
+}
+
+impl PartialEq for Duration {
+  fn eq(&self, other: &Duration) -> bool {
+    self.to_millis() == other.to_millis()
+  }
 }
 
 fn main() {
