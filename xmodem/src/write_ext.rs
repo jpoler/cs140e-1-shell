5194601ad349560310579cd4635fe731a53e7f45
Jon Poler <jonathan.poler@gmail.com>
2018-09-02 19:01:39 -0700

    WriteExt trait

---
+++ xmodem/src/write_ext.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+pub trait WriteExt: io::Write {
+    fn write_max(&mut self, mut buf: &[u8]) -> io::Result<usize> {
+        let start_len = buf.len();
+
+        while !buf.is_empty() {
+            match self.write(buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    let tmp = buf;
+                    buf = &tmp[n..];
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+                Err(e) => panic!(e),
+            }
+        }
+
+        self.flush().unwrap();
+        Ok(start_len - buf.len())
+    }
+}
+
+impl<T: io::Write> WriteExt for T {}
